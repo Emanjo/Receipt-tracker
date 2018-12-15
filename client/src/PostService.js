898d8f0f4612e2f73005f309b3f6bab0573b9252
Emil Johannessen <emil.jona@gmail.com>
2018-12-15 18:06:27 +0100

    Added client code

---
+++ client/src/PostService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const url = 'api/posts/';
+
+class PostService {
+	//Get posts
+	static getPosts() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const res = await axios.get(url);
+				const data = res.data;
+				resolve(
+					data.map(post => ({
+						...post,
+						createdAt: new Date(post.createdAt)
+					}))
+				);
+			} catch (err) {
+				reject(err);
+			}
+		})
+	}
+
+	//Create post
+	static insertPost(text) {
+		return axios.post(url, {
+			text
+		})
+	}
+
+	//Delete post
+	static deletePost(id) {
+		return axios.delete(`${url}${id}`)
+	}
+}
+
+export default PostService;
