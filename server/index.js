88ff11ab4873b92187082a3c6336473946877ff4
Emil Johannessen <emil.jona@gmail.com>
2018-12-15 18:00:48 +0100

    Added production client

--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+// Production
+
+if (process.env.NODE_ENV === 'production') {
+	//Static folder
+	app.use(express.static(__dirname + '/public'));
+
+	//Handle single page application
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
