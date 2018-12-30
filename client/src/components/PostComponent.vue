26a4a58ec045b63d6d9d31e33e90ca230a849dc6
Emil Johannessen <emil.jona@gmail.com>
2018-12-31 00:27:15 +0100

    Added support for SASS

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -59,41 +59,47 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-div.container {
-  max-width: 800px;
-  margin: 0 auto;
+<style lang="scss" scoped>
+
+div {
+  &.container {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  &.post {
+    position: relative;
+    border: 1px solid green;
+    background-color: lightblue;
+    padding: 10px 10px 30px 10px;
+    margin-bottom: 15px;
+  }
+
+  &.created-at {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 5px 15px 5px 15px;
+    background-color: darkgreen;
+    color: white;
+    font-size: 13px;
+  }
 }
 
-p.error {
-  border: 1px solid red;
-  background-color: pink;
-  padding: 10px;
-  margin-bottom: 15px;
-}
+p {
+  &.error {
+    border: 1px solid red;
+    background-color: pink;
+    padding: 10px;
+    margin-bottom: 15px;
+  }
 
-div.post {
-  position: relative;
-  border: 1px solid green;
-  background-color: lightblue;
-  padding: 10px 10px 30px 10px;
-  margin-bottom: 15px;
-}
+  &.text {
+    font-size: 22px;
+    font-weight: 700;
+    margin-bottom: 0;
+  }
 
-div.created-at {
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 5px 15px 5px 15px;
-  background-color: darkgreen;
-  color: white;
-  font-size: 13px;
-}
-
-p.text {
-  font-size: 22px;
-  font-weight: 700;
-  margin-bottom: 0;
 }
 
 </style>
