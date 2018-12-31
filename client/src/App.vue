8ea36404343e2d61ccceabdba3f95401c1c23c10
Emil Johannessen <emil.jona@gmail.com>
2018-12-31 01:12:36 +0100

    Added reset to style

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <PostComponent msg="Welcome to Your Vue.js App"/>
+    <PostComponent/>
   </div>
 </template>
 
@@ -15,14 +15,24 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+@import 'assets/reset';
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  height: 100%;
+}
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  padding: 30px;
 }
 </style>
