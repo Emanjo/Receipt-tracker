956a8b78395dd8475c3b07caa2fa7422108afc48
Emil Johannessen <emil.jona@gmail.com>
2019-02-02 21:31:22 +0100

    90% finished with the 'add new receipt'-form

--- client/src/components/NewReceipt.vue
+++ client/src/components/NewReceipt.vue
@@ -5,14 +5,30 @@
 
       <form action="post" class="form">
 
-        <div class="form__group">
-          <label for="item">Item:</label>
-          <input type="text" name="item" id="item">
+        <div class="form__group form__group--items">
+          <table>
+            <tr>
+              <th><label for="retailer">Retailer:</label></th>
+            </tr>
+            <tr>
+              <td><input class="margin-bottom" name="item" id="retailer"></td>
+            </tr>
+            <tr>
+              <th><label for="description">Description:</label></th>
+              <th><label for="price">Price per item:</label></th>
+              <th><label for="count">Count:</label></th>
+            </tr>
+            <tr v-for="input in tableRowInputs">
+              <td v-html="input.description"></td>
+              <td v-html="input.price"></td>
+              <td v-html="input.count"></td>
+            </tr>
+          </table>
         </div>
-
+        
         <div class="form__group">
-          <label for="price">Price:</label>
-          <input type="text" name="price" id="price">
+          <button type="button" v-on:click="removeItem" class="form__button form__button--modifier form__button--minus">-</button>
+          <button type="button" v-on:click="addItem" class="form__button form__button--modifier form__button--plus">+</button>
         </div>
 
         <input class="form__button form__button--submit" type="submit" value="Add"/>
@@ -25,6 +41,26 @@
 
 <script>
   export default {
+    data() {
+      return {
+        tableRowInputs: [{description: '<input name="item">', 
+                          price: '<input type="number">',
+                          count: '<input type="number">'}]
+      }
+    },
+    methods: {
+      addItem() {
+        this.tableRowInputs.push({description: '<input name="item">', 
+                          price: '<input type="number">',
+                          count: '<input type="number">'});
+      },
+      removeItem() {
+        this.tableRowInputs.splice(-1,3);
+      }
+    },
+    mounted() {
+      
+    }
     
   }
 </script>
@@ -36,20 +72,28 @@
   width: 70%;
   flex-wrap: wrap;
   margin: auto;
-  justify-content: center;
+  justify-content: space-around;
   margin-top: 40px;
 
-  input[type=text] {
-    border-radius: 5px;
-    border: 1px solid black;
-    padding: 3px;
-  }
-
   &__group {
-    margin: 0 10px;
-    label {
-      margin-right: 5px;
+    margin: 10px 0px;
+    width: 40%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    &--items {
+      width: 100%;
+      label {
+        display: block;
+        margin-bottom: 10px;
+      }
+      td {
+        padding: 10px;
+        text-align: center;
+      }
     }
+
   }
 
   &__button {
@@ -60,11 +104,30 @@
     padding: 5px;
 
     &--submit {
-      background: green;
+      background: #00e300;
       color: white;
-      width: 150px;
-      flex-basis: 100%;
+      flex-basis: 70%;
     }
+
+    &--modifier {
+      width: 40px;
+      margin: 0 10px;
+      color: white;
+      font-weight: bold;
+      font-size: 16px;
+    }
+
+    &--minus {
+      background: red;
+    }
+
+    &--plus {
+      background: #00e300;
+    }
+  }
+
+  .margin-bottom {
+    margin-bottom: 50px;
   }
 
 }
