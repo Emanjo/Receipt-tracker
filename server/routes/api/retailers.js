4caec1796db3742eb6409b0e413524a5fb67c0db
Emanjo <emil.jona@gmail.com>
2019-02-19 18:56:51 +0100

    Added new sum endpoint to retailer

--- server/routes/api/retailers.js
+++ server/routes/api/retailers.js
@@ -5,13 +5,32 @@ const router = express.Router();
 
 //Get all retailers
 router.get('/', async (req, res) => {
-	const collection = await loadRetailersCollection(), allRetailers = await collection.find({}).toArray();
+	const collection = await loadRetailersCollection(), data = await collection.find({}).toArray();
 	let meta = {};
 
 	// Add the numbers of retailers to metadata
-	meta["numberOfRetailers"] = allRetailers.length;
+	meta["numberOfRetailers"] = data.length;
 
-	res.send({ data: allRetailers, meta: meta });
+	res.send({ data: data, meta: meta });
+});
+
+//Get all retailers and total money spent on each based on a certain time like 7 days, 30 days etc.
+router.get('/sum', async (req, res) => {
+	const collection = await loadRetailersCollection(), data = await collection.find({}).toArray();
+	const receiptsCollection = await loadReceiptsCollection(), receiptsData = await receiptsCollection.find({}).toArray();
+	let meta = {};
+
+	 //Adds sums to each object in data
+	 data.forEach( retailer => {
+		retailer.sum = 0;
+		receiptsData.forEach( receipt => {
+		  if(receipt.retailer === retailer.name) {
+			retailer.sum += receipt.sum;
+		  }
+		});
+	  });
+
+	res.send({ data: data, meta: meta });
 });
 
 async function loadRetailersCollection() {
@@ -21,6 +40,15 @@ async function loadRetailersCollection() {
 	});
 
 	return client.db(process.env.MONGO_DB).collection('retailers');
+};
+
+async function loadReceiptsCollection() {
+	const client = await mongodb.MongoClient
+		.connect(process.env.MONGO_DB_URI, {
+			useNewUrlParser: true
+		});
+
+	return client.db(process.env.MONGO_DB).collection('receipts');
 };
 
 module.exports = router;
